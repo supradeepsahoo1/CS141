c39db62456b19e1cee740fc36ce9836c93e15539
supradeepsahoo1 <31537634+supradeepsahoo1@users.noreply.github.com>
2017-09-01 17:21:47 +0530

    Create lab1pr5.cpp

---
+++ lab1pr5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  double x, y;
+  cout << "enter the base and then the index";
+  cin << x << y;
+  cout << "value of " << x << " raised to the power of " << y << ": " << pow(x, y);
+}
