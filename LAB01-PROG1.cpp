10817c7fdcd71b18dafd09a3a1ca4de290ad0e23
supradeepsahoo1 <31537634+supradeepsahoo1@users.noreply.github.com>
2017-09-01 16:20:56 +0530

    Create LAB01-PROG1.cpp

---
+++ LAB01-PROG1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a;
+  float b;
+  char c;
+  double d;
+  bool e;
+  cout << "Enter an integer:";
+  cin >> a;
+  cout << "Enter a floating-point decimal:";
+  cin >> b;
+}
