a4548b084781585b22e12971eb899856a958da68
supradeepsahoo1 <31537634+supradeepsahoo1@users.noreply.github.com>
2017-09-01 16:49:29 +0530

    Create lab2pr3.cpp

---
+++ lab2pr3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using name space std
+void main()
+{
+ float cel, far;
+  clrscr();
+  cout<<"Enter temp. in Fahrenheit: ";
+  cin>>far;
+  cel = (far - 32) * 5/9;
+  cout<<"Temp. in Celsius: "<<cel;
+ getch();
+}
