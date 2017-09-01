daa5b565036de46f2adde0f6516beb2661651277
supradeepsahoo1 <31537634+supradeepsahoo1@users.noreply.github.com>
2017-09-01 16:57:18 +0530

    Create lab2pr8.cpp

---
+++ lab2pr8.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+void main()
+{
+float height, base;
+double area;
+cout<<"Enter Base : ";
+cin>>base;
+cout<<"Enter Height : ";
+cin>>height;
+area=0.5*base*height;
+cout<<"\nArea of Triangle  :"<getch();
+}
