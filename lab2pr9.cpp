5c4424be5e18ae62f587d612c7e4ee170439f556
supradeepsahoo1 <31537634+supradeepsahoo1@users.noreply.github.com>
2017-09-01 16:58:54 +0530

    Create lab2pr9.cpp

---
+++ lab2pr9.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+void main()
+{
+//clear the screen.
+clrscr();
+//declare variable type float
+float a,b,area;
+//Input the side and save it in 'a'
+cout<<"Enter the side of Equilateral Triangle"<<endl;
+cin>>a;
+cout<<"Enter the height of Equilateral Triangle"<<endl;
+cin>>b;
+//calculate area and save it in 'area'
+area=(a*b)/2;
+//show the output 'area'
+cout<<"Area of  Equilateral Triangle is "<<area;
+//get character
+getch();
+}
